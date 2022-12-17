b2dc54850e5a3afa585d2f4103d2740a81626b2b
jonasbfranco <jonasbfranco@hotmail.com>
2022-12-17 11:48:05 -0300

    :tada: First commit

---
+++ client/src/Table.jsx
@@ -0,0 +1,31 @@
+// import React from 'react';
+// import img from 'react-image'
+import Avatar_Female from './img/avatar/avatar_female.svg';
+import Avatar_Male from './img/avatar/avatar_male.svg';
+
+
+
+const Table = ({ data }) => {
+  return (
+    <table>
+      <tbody>
+        <tr className='tr_cabecalho'>
+          <th>Profile</th>
+          <th>Name</th>
+          <th>Surname</th>
+          <th>Email</th>
+        </tr>
+        {data.map((item) => (
+          <tr className='tr_contatos' key={item.id}>
+            <td>{item.gender == 'Female' ? <img src={Avatar_Female}/> : <img src={Avatar_Male}/> }</td>
+            <td>{item.first_name}</td>
+            <td>{item.last_name}</td>
+            <td>{item.email}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+};
+
+export default Table;
